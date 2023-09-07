ed1fcd0a988ff1184363f28caaad62d7af67f3b4
yetimoreno777 <124410746+yetimoreno777@users.noreply.github.com>
2023-09-06 19:15:09 -0700

    Practica 3 06/09/2023

--- parcial1/Practica3_js.js
+++ parcial1/Practica3_js.js
@@ -1,7 +1,31 @@
 $(document).ready(function(){
-    var contador = 0;
+    var carta1 = ""; var carta2 = "";
+    var par = false;
+    var total_pares = 0;
+
     $('img').click(function(e){
+        var estado = $(this).attr('data-estado');
         var nombre_imagen = $(this).attr('data-id');
-        $(this).attr('src', 'imagenes/'+ nombre_imagen);
+
+        if(estado == "0"){
+            if(carta1 == ""){
+                carta1 = $(this);
+                carta1.attr('src', 'imagenes/' + nombre_imagen);
+                console.log("Se asigno carta #1");
+            } else {
+                carta2 = $(this);
+                carta2.attr('src', 'imagenes/' + nombre_imagen);
+                console.log("Se asigno carta #2");
+            }
+        }
+        if(carta1 != "" && carta2 !=""){
+            if(carta1.attr('data-id')==carta2.attr('data-id')){
+                par=true;
+                total_pares++;
+                $("#total_pares").html(total_pares);
+                carta1 = "";
+                carta2 = "";
+            }
+        }
     });
 });
